1fc6b150345f9409568bb8d912d3215dfaab3912
Youssef Ahmed <youssef01120646586@gmail.com>
2025-12-14 13:18:16 +0200

    initiate module and manifest file

---
+++ nesting_optimizer/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Nesting Optimizer Engine',
+    'author': 'Youssef Ahmed',
+    'version': '1.0',
+    'category': 'Manufacturing',
+    'summary': 'Optimize cutting of material pieces',
+    'description': """
+        This module integrates Nest2D C++ library for cutting optimization.
+    """,
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
